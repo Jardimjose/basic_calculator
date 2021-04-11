767e33c3af9cde32892f7405f3168f1927675fb8
Jardimjose <josejardimdev@gmail.com>
2021-04-11 11:17:41 +0200

    updated some functionality, will improve more.

--- index.js
+++ index.js
@@ -26,5 +26,33 @@ keys.addEventListener('click', (event) => {
         return;
     }
 
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
 
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('all-clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
 })
+
+function inputDigit(digit) {
+    const { displayValue } = calculator;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot
+    }
+}
